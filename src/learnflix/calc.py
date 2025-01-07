836d599e5efa71d133c4ceb810e36aacfd349e25
Damian Pomykała <168227269+dpomykala@users.noreply.github.com>
2025-01-07 14:11:41 +0100

    Add a basic test setup with pytest

---
+++ src/learnflix/calc.py
@@ -0,0 +1,3 @@
+def add(first_num: int, second_num: int) -> int:
+    return first_num + second_num
+
---
+++ tests/test_calc.py
@@ -0,0 +1,6 @@
+from learnflix.calc import add
+
+
+def test_adding_two_numbers():
+    assert add(1, 5) == 6
+
